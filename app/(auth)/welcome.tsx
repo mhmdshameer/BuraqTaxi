ab204f8468f455f8655ee42d2b68e805e68650a3
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-08 10:13:32 +0530

    UI(welcome): make welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,37 @@
-import { Text, TouchableOpacity } from "react-native";
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import { useRef } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
-      <TouchableOpacity>
-        <Text className="text-primary-500 font-JakartaBold">
-          Skip
-        </Text>
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-base font-JakartaBold">Skip</Text>
       </TouchableOpacity>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full " />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full " />
+        }
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex justify-center items-center">
+            <Image source={item.image} className="w-full h-[300px]" resizeMode="contain" />
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
