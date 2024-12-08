876b87e266b2879ef096d19ccb0d3fdd1f6c48eb
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-08 17:11:53 +0530

    feat(SignIn): create functional signIn page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,14 +6,13 @@ import { Link } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
-const SignUp = () => {
+const SignIn = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
   });
 
-  const onSignUpPress = async () => {};
+  const onSignInPress = async () => {};
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -21,18 +20,11 @@ const SignUp = () => {
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px] " />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-            Create Your Account
+            Welcome back
           </Text>
         </View>
 
         <View className="p-5">
-          <InputField
-            label="Name"
-            placeholder="Enter your name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value) => setForm({ ...form, name: value })}
-          />
           <InputField
             label="Email"
             placeholder="Enter your email"
@@ -50,24 +42,23 @@ const SignUp = () => {
 
           <CustomButton
             title="Sign Up"
-            onPress={onSignUpPress}
+            onPress={onSignInPress}
             className="mt-6"
           />
 
           <OAuth/>
 
           <Link
-            href="/sign-in"
+            href="/sign-up"
             className="text-lg text-center text-general-200 mt-10"
           >
-            Already have an account?{" "}
-            <Text className="text-primary-500">Log In</Text>
+            Don't have an account?{" "}
+            <Text className="text-primary-500">Sign Up</Text>
           </Link>
-
         </View>
       </View>
     </ScrollView>
   );
 };
 
-export default SignUp;
+export default SignIn;
