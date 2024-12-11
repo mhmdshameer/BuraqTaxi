979bc4a3251ece666acaa54ea35da408b5494cd0
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-11 16:18:28 +0530

    feat(store): create zustand store

--- components/Map.tsx
+++ components/Map.tsx
@@ -3,15 +3,17 @@ import { View } from 'react-native';
 import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
 
 const Map = () => {
+    // const region = {}
     return (
         <View className='w-full h-full rounded-2xl'>
             <MapView
                 provider={PROVIDER_DEFAULT}
                 style={{ flex: 1,  }}
-                
                 tintColor='black'
                 mapType='mutedStandard'
                 showsPointsOfInterest={false}
+                // initialRegion={region}
+                showsUserLocation={true}
             />
         </View>
     );
