9b89ad303248ca13e87c9abc1e34c55424cad625
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-11 11:04:17 +0530

    feat(RideCard): give other details

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -47,6 +47,19 @@ const RideCard = ({
             <Text className=" text-base font-JakartaMedium text-gray-500">Date & Time</Text>
             <Text className=" text-base font-JakartaMedium text-gray-500">{formatDate(created_at)}, {formatTime(ride_time)}</Text>
         </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className=" text-base font-JakartaMedium text-gray-500">Driver</Text>
+            <Text className=" text-base font-JakartaMedium text-gray-500">{driver.first_name} {driver.last_name}</Text>
+        </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className=" text-base font-JakartaMedium text-gray-500">Car Seats</Text>
+            <Text className=" text-base font-JakartaMedium text-gray-500">{driver.car_seats}</Text>
+        </View>
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+            <Text className=" text-base font-JakartaMedium text-gray-500">Payment Status</Text>
+            <Text className={`text-base capitalize font-JakartaMedium ${payment_status === "paid" ? "text-green-500": "text-red-500"}`}>{payment_status}</Text>
+        </View>
+        
       </View>
     </View>
   </View>
