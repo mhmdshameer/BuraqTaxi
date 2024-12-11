6cb1116dadd9981878f0b45a2080a004dc1d01bd
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-11 10:33:34 +0530

    feat(RideCard): add addresses of ride

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,3 +1,4 @@
+import { icons } from "@/constants";
 import { Ride } from "@/types/type";
 import { Image, Text, View } from "react-native";
 
@@ -18,10 +19,29 @@ const RideCard = ({
   <View className="flex flex-row justify-center items-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
     <View className="flex flex-row items-center justify-between p-3">
       <View className="flex flex-row items-center justify-between">
-        <Image source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,}} className="w-[80px] h-[90px] rounded-lg" />
+        <Image
+          source={{
+            uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+          }}
+          className="w-[80px] h-[90px] rounded-lg"
+        />
+
+        <View className="flex flex-col mx-5 gap-y-5 flex-1">
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.to} className="w-5 h-5" />
+            <Text className="text-base font-JakartaMedium" numberOfLines={1}>
+              {origin_address}
+            </Text>
+          </View>
+          <View className="flex flex-row items-center gap-x-2">
+            <Image source={icons.point} className="w-5 h-5" />
+            <Text className="text-base font-JakartaMedium" numberOfLines={1}>
+              {destination_address}
+            </Text>
+          </View>
+        </View>
       </View>
     </View>
-    <Text className="text-3xl">{driver.first_name}</Text>
   </View>
 );
 
