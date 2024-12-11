b857dbd53fdce25e45dad33330d0f4b090297154
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-12-11 10:25:03 +0530

    feat(RideCard): show map of the ride

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,10 +1,28 @@
-import { Ride } from "@/types/type"
-import { Text, View } from "react-native"
+import { Ride } from "@/types/type";
+import { Image, Text, View } from "react-native";
 
-const RideCard = ({ride}:{ride:Ride}) => (
-    <View>
-        <Text>{ride.driver.first_name}</Text>
+const RideCard = ({
+  ride: {
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
+  <View className="flex flex-row justify-center items-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-row items-center justify-between">
+        <Image source={{ uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,}} className="w-[80px] h-[90px] rounded-lg" />
+      </View>
     </View>
-)
+    <Text className="text-3xl">{driver.first_name}</Text>
+  </View>
+);
 
-export default RideCard
+export default RideCard;
